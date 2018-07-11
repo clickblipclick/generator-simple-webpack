8452153e8aec07b5f3b5ccdd67c5025be74a1747
Ben Nyberg <ben.f.nyberg@gmail.com>
2018-07-11 18:33:39 -0400

    add file/url-loaders for images, add clean-webpack-plugin, various minor fixes

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const devConfig = require("./cfg/webpack.config.dev");
-const buildConfig = require("./cfg/webpack.config.build");
+const devConfig = require("./webpack.config.dev");
+const buildConfig = require("./webpack.config.build");
 
 const commonConfig = {
   entry: "./src/scripts/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "scripts/bundle.[hash].js"
+    filename: "scripts/bundle.[hash].js",
+    publicPath: "/"
   },
   plugins: [new HtmlWebpackPlugin({
     template: 'src/index.html'
@@ -17,7 +18,20 @@ const commonConfig = {
     rules: [
       { test: /\.js$/, use: [<% if (includeBabel) { %>"babel-loader", <% } %>"eslint-loader"], exclude: /node_modules/ },
       { test: /\.css$/, use: ["style-loader", "css-loader", "postcss-loader"] }<% if (includeSass) { %>,
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"] }<% } %>
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"] }<% }, %>
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "[name]-[hash].[ext]",
+              outputPath: "images/",
+              limit: 8192
+            }
+          }
+        ]
+      }
     ]
   }
 };
